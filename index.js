cf0882c034ba55b5793dba1b4614d0b3b65530af
ibgdev <ibrahimghorbali605@gmail.com>
2025-10-17 20:56:12 +0100

    architecture

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 const app = express();
-const PORT = 3200;
+const PORT = process.env.PORT || 3200
 
 // Middleware pour JSON
+app.use(cors());
 app.use(express.json());
 
 // Route test
