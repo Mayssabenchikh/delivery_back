62e17d8cf29db67622c212444fcd382c480240ef
ibgdev <ibrahimghorbali605@gmail.com>
2025-11-01 20:55:29 +0100

    Cleanup: stop tracking node_modules, .history, and .env

--- .history/src/routes/auth.routes_20251022205334.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/auth.controller');
-const { validateSignUp } = require('../middlewares/validation.middleware');
-
-// POST /api/auth/signup
-router.post('/signup', validateSignUp, authController.signUp);
-
-module.exports = router;
