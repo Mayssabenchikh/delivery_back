795f4aad15228b1dc547b4cf317de0b9f2079bb5
ibgdev <ibrahimghorbali605@gmail.com>
2025-11-03 12:38:12 +0100

    merged login

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+export const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1]; // "Bearer <token>"
+
+  if (!token) return res.status(401).json({ message: 'Token manquant' });
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return res.status(403).json({ message: 'Token invalide' });
+    req.user = user; // contient { id, email, role }
+    next();
+  });
+};
+
+
+export const authorizeRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({ message: 'Accès refusé : rôle non autorisé' });
+    }
+    next();
+  };
+};
